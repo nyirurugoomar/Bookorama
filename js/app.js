bbb9a28c474517cdbcd5b5e72aa766b61b259360
omar <nyirurugoomar@gmail.com>
2022-02-04 17:18:00 +0200

    delete function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-var btns = document.querySelectorAll('#book-list .delete');
+const list = document.querySelector('#book-list ul');
 
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click',function(e){
-        const li = e.target.parentElement;
-
-        li.parentNode.removeChild(li);
-    });
-});  
+list.addEventListener('click',function(e){
+    if(e.target.className =='delete'){
+        const li= e.target.parentElement;
+        list.removeChild(li)
+    }
+})
