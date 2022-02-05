b0bc02d62bcc54091dd87de02807a752092e26a6
omar <nyirurugoomar@gmail.com>
2022-02-05 13:28:01 +0200

    adding book list button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+  // delete button books
 const list = document.querySelector('#book-list ul');
 
 list.addEventListener('click',function(e){
@@ -5,4 +6,34 @@ list.addEventListener('click',function(e){
         const li= e.target.parentElement;
         list.removeChild(li)
     }
+});
+
+//add book-list
+
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value =addForm.querySelector('input[type="text"]').value;
+
+  //create elements
+
+const li = document.createElement('li');
+const bookName = document.createElement('span');
+const deleteBtn = document.createElement('span');
+
+// add content
+deleteBtn.textContent=['delete'];
+bookName.textContent=value;
+
+//ADD CLASSES
+bookName.classList.add('name');
+deleteBtn.classList.add('delete');
+
+
+// append to document 
+li.appendChild(bookName);
+li.appendChild(deleteBtn);
+list.appendChild(li);
+
+
 })
