4e868737be2f5c7dd5e5c57924d8139038dfe8b1
omar <nyirurugoomar@gmail.com>
2022-02-11 14:44:47 +0200

    removing tabbed-content

--- js/app.js
+++ js/app.js
@@ -60,23 +60,24 @@ searchBar.addEventListener('keyup', function(e){
 })
 
 //tabbed content
-const tabs = document.querySelector('tabs');
-const panels = document.querySelectorAll('panel');
+// const tabs = document.querySelector('tabs');
+// const panels = document.querySelectorAll('panel');
 
-tabs.addEventListener('click',function(e){
+// tabs.addEventListener('click',function(e){
   
-  if(e.target.tagName == "LI"){
+//   if(e.target.tagName == "LI"){
 
-    const targetPanel = document.querySelector(e.target.dataset.target);
-     panels.forEach(function(panel){
-       if(panel == targetPanel){
-         panel.classList.add('active');
-       }else{
-         panel.classList.remove('active');
-       }
-     })
-  }
-})
+//     const targetPanel = document.querySelector(e.target.dataset.target);
+//      panels.forEach(function(panel){
+//        if(panel == targetPanel){
+//          panel.classList.add('active');
+//        }else{
+//          panel.classList.remove('active');
+//        }
+//      })
+//   }
+// })
 
 
 
+}); 
